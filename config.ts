b18ec79084778ba74508e49fe115cb690a90cc75
Sumanth Perambuduri <sumanthpera3@gmail.com>
2022-06-09 13:51:46 +0530

    re-written in typescript

---
+++ config.ts
@@ -0,0 +1,9 @@
+const categoryName = "Create Voice Channel"
+const channels = [
+    ["Create Duo", "New Duo", 2],
+    ["Create Trio", "New Trio", 3],
+    ["Create Quad", "New Quad", 4],
+    ["Create Penta", "New Penta", 5]
+]
+
+export { categoryName, channels }
